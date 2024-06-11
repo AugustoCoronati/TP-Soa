1cc1e397366fab7af192d5c62af8e9e54e6835fe
AugustoCoronati <augustocoronati@hotmail.com>
2024-06-10 21:00:53 -0300

    Navegacion

--- Android/EcoSwitch/app/src/main/java/com/example/ecoswitch/MainActivity.java
+++ Android/EcoSwitch/app/src/main/java/com/example/ecoswitch/MainActivity.java
@@ -1,19 +1,74 @@
 package com.example.ecoswitch;
 
+//
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+//
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnIngresar;
+
+    private ProgressDialog mProgressDlg;
+
+    private ArrayList<BluetoothDevice> mDeviceList = new ArrayList<BluetoothDevice>();
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    public static final int MULTIPLE_PERMISSIONS = 10; // code you want.
+
+    //se crea un array de String con los permisos a solicitar en tiempo de ejecucion
+    //Esto se debe realizar a partir de Android 6.0, ya que con verdiones anteriores
+    //con solo solicitarlos en el Manifest es suficiente
+    String[] permissions= new String[]{
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        btnIngresar = (Button) findViewById(R.id.button2);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -22,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
     }
 
     public void goToSecondActivity(View view) {
